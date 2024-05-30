50f76513c69b0670cdac6cd59b41a34e3caaf7f7
barath <82313227+Barath-2001@users.noreply.github.com>
2024-05-30 14:49:08 +0530

    Update Home.py

--- Application/Home.py
+++ Application/Home.py
@@ -52,7 +52,10 @@ if selected=='Home':
         rej_df.set_index('MONTH',inplace=True)   
         st.header("Analysis")  
         fig,ax=plt.subplots(figsize=(18,6))                                                                                         
-        sns.countplot(df_main['TRANSACTION_TYPE'])
+        sns.countplot(x=df_main['TRANSACTION_TYPE'])
+        arr=list(df_main['TRANSACTION_TYPE'].value_counts())                                                                        
+        for i ,v in enumerate(arr):
+            plt.text(i,v,str(v),ha='center')
         st.pyplot(fig)
         key=['ACCEPTED','REJECTED']
         values=[]
