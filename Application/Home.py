d3bcf640d8c16716ffeba0ac1cf782927ccef586
barath <82313227+Barath-2001@users.noreply.github.com>
2025-02-19 12:14:22 +0530

    Update Home.py

--- Application/Home.py
+++ Application/Home.py
@@ -34,6 +34,7 @@ if selected=='Home':
         df_main['ITEM_ID'].fillna(-1,inplace=True)
         if df_main['ITEM_ID'].dtype != 'O':
             df_main['ITEM_ID']=pd.to_numeric(df_main['ITEM_ID'], downcast='integer', errors='coerce') 
+        st.write(df_main.dtypes)
         df_main['TRANSACTION_DATE']=pd.to_datetime(df_main['TRANSACTION_DATE'].dt.normalize()).copy()
         acpt_df=df_main.loc[(df_main['TRANSACTION_TYPE']=='ACCEPT') & (df_main['ITEM_ID']!=-1)].copy()
         a=pd.to_datetime(acpt_df['TRANSACTION_DATE'])                                                                               
