13eb383820dd8dd930cb932dc5d25d356d1ff78c
barath <82313227+Barath-2001@users.noreply.github.com>
2025-02-19 13:03:07 +0530

    Update Home.py

--- Application/Home.py
+++ Application/Home.py
@@ -26,7 +26,7 @@ if selected=='Home':
         def read_data(file):
             po_receiving_data=pd.read_excel(file,na_values='Missing',usecols="C,F,M,O:P",engine='openpyxl')
             st.toast('File upload successfully.', icon="✅")
-            time.sleep(6)
+            
             # st.success("Items with no ID are omitted") 
             return po_receiving_data
         po_receiving_data=read_data(file)
