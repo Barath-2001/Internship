2cc5c476c00275169c2d9e2beabf7ff886b6960f
barath <82313227+Barath-2001@users.noreply.github.com>
2025-03-10 15:12:40 +0530

    Update Home.py

--- Application/Home.py
+++ Application/Home.py
@@ -63,7 +63,10 @@ if 'flag' not in st.session_state:
 
 
 def Rejection_rate(inp3,inp4):
-    df=df_main.loc[(df_main['VENDOR_ID']==inp3)&(df_main['ITEM_ID'].isin(inp4))].sort_values(by=['TRANSACTION_DATE','PO_LINE_ID']).copy()
+    if inp3:
+        df=df_main.loc[(df_main['VENDOR_ID']==inp3)&(df_main['ITEM_ID'].isin(inp4))].sort_values(by=['TRANSACTION_DATE','PO_LINE_ID']).copy()
+    else:
+        df=df_main.loc[(df_main['ITEM_ID']==inp4)].sort_values(by=['TRANSACTION_DATE','PO_LINE_ID']).copy()
     qn=dict(df.loc[df['TRANSACTION_TYPE']=='RECEIVE'].groupby(['VENDOR_ID','ITEM_ID','PO_LINE_ID'])['ACTUAL_QUANTITY'].sum())
     rej_rate=[]
     tot_qn={}
