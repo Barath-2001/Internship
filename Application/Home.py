153af6c1498d521e591b62d5b497e8220ec346ea
barath <82313227+Barath-2001@users.noreply.github.com>
2025-03-12 15:20:44 +0530

    Update Home.py

--- Application/Home.py
+++ Application/Home.py
@@ -385,7 +385,7 @@ if selected=='Home':
                     lambda x: f"🟢 {x}%" if float(x) >= 95.0 else 
                              (f"🟡 {x}%" if float(x) >= 80.0 and float(x) < 95.0 else f"🔴 {x}%").copy()
                 )
-                 temp_df["REJECTION RATE"] = temp_df["REJECTION RATE"].apply(
+                temp_df["REJECTION RATE"] = temp_df["REJECTION RATE"].apply(
                     lambda x: f"🟢 {x}%" if float(x) <= 5.0 else 
                              (f"🟡 {x}%" if float(x) > 5.0 and float(x) < 10.0 else f"🔴 {x}%").copy()
                 )
